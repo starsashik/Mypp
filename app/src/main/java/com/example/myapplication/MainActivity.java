ebad5b23bc345e4261c715ede2540cc923b20f5f
Alexander <alexanderlbz@mail.ru>
2022-01-19 18:41:22 +0300

    Error created

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,7 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String A = "";
+        String 1 = 123;
+        int sad = "";
 
     }
     class abss{}
+
 }
